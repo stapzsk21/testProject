b537d9e951dd40d262059be96e3a5c7b5037eeb1
VasilyiKa <VasilyiKa@terra.lan>
2026-02-11 18:52:16 +0200

    fixed

--- src/components/layouts/404/NotFoundHeader.jsx
+++ src/components/layouts/404/NotFoundHeader.jsx
@@ -1,17 +1,9 @@
 import { Link } from 'react-router-dom';
 
-import Lottie from 'lottie-react';
 
-import NotFoundHeaderImage from '../../../assets/images/header-404-image.png';
-import testAnimation from '../../../assets/json/lottie/header-animation.json';
 import '../../../assets/styles/header.scss';
 import { APP_CONSTANTS } from '../../../constants/site.config';
 import { useTranslation } from '../../functions/translate/TranslationContext';
-// import DecorLines from '../mixins/DecorLines.jsx';
-import TypingUnderlineHeading from '../mixins/TypingUnderlineHeading';
-// import SquareDecoration from '../mixins/SquareDecoration.jsx';
-import sugar1 from '../../../assets/images/sugar1.svg';
-import sugar2 from '../../../assets/images/sugar2.svg';
 
 function NotFoundHeader() {
   const { translations } = useTranslation();
@@ -21,53 +13,9 @@ function NotFoundHeader() {
     <header className="page-header not-found">
       <div className="wrapper">
         <div className="page-header__inner flex">
-          <div className="page-header__info">
-            <TypingUnderlineHeading as="h1" className="h1-title page-header__title" speed={70}>
-              {notFoundHeaderTitle}
-            </TypingUnderlineHeading>
-
-            <p className="page-header__text text-baner">{notFoundHeaderText}</p>
-            <Link className="btn btn-second" to="/">
-              <span>{buttonTextMain}</span>
-            </Link>
-          </div>
-
-          <div
-            className={`page-header__image${APP_CONSTANTS.isAnimated ? ' animation-page-header__image' : ''}`}
-          >
-            {APP_CONSTANTS.isAnimated ? (
-              <Lottie
-                className="page-header-image animation-image"
-                animationData={testAnimation}
-                loop={true}
-              />
-            ) : (
-              <img src={NotFoundHeaderImage} className="page-header-image" />
-            )}
-
-            <div className="square-decoration__container">
-                {/* <SquareDecoration
-                  style={{
-                    left: 0,
-                    top: '56%',
-                  }}
-                  icon={sugar1}
-                  speedAnimateOpacity={800}
-                />
-
-                <SquareDecoration
-                  style={{
-                    right: 0,
-                     top: '56%',
-                  }}
-                  icon={sugar2}
-                  speedAnimateOpacity={700}
-                /> */}
-            </div>
-          </div>
+          404
         </div>
       </div>
-      {/* <DecorLines /> */}
     </header>
   );
 }
